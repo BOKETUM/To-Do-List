e5df338073c6bb1a000b5c8c09ababd683c4ed85
Supicha Sritrakul <spimpm@gmail.com>
2017-10-13 16:07:59 +0700

    Code

---
+++ Sprint 1 Code,DB,Config/todolistSQL.sql
@@ -0,0 +1,38 @@
+﻿--
+-- Database: `todolist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list`
+--
+
+CREATE TABLE `list` (
+  `Title` varchar(26) NOT NULL,
+  `Date` date DEFAULT NULL,
+  `Detail` text,
+  `Status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `list`
+--
+
+INSERT INTO `list` (`Title`, `Date`, `Detail`, `Status`) VALUES
+('test1', '2017-09-08', 'Test2', 1),
+('test3', '2017-09-01', '', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `list`
+--
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`Title`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
